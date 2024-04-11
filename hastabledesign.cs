2c956d4763ae251293379e4f2162f6cfeec21f13
Margaret Sitati (WTS International Limited) <v-masitati@microsoft.com>
2024-04-11 14:01:31 +0300

    Adding the code template

---
+++ hastabledesign.cs
@@ -0,0 +1,19 @@
+public class MyHashMap {
+
+    public MyHashMap() {
+
+        
+    }
+    
+    public void Put(int key, int value) {
+        
+    }
+    
+    public int Get(int key) {
+        
+    }
+    
+    public void Remove(int key) {
+        
+    }
+}
